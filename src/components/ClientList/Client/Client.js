497abdfc29630ac6f9279c2409af7ac7eb12b41e
Mayank <hellomayankashok@gmail.com>
2021-02-17 14:21:43 -0800

    Add buttons to Client component
    - added buttons (treatment details and payment details) to right section of client details row in Client.js component.. added some css rules as well.
    
    Next :
            - create a custom link and/or button component for reuse

--- src/components/ClientList/Client/Client.js
+++ src/components/ClientList/Client/Client.js
@@ -5,8 +5,8 @@ import classes from './Client.module.css'
 export default function Client(props) {
     return (
         <div className={classes.Client}>
-            
-            <div className={classes.Client_div_name_age_gender}>
+
+            <div className={classes.Client_name_age_gender_div}>
                 <p><small><strong>{props.name}</strong></small></p>
                 <div>
                     <p><small><strong>{props.age} years</strong></small></p>
@@ -14,12 +14,24 @@ export default function Client(props) {
                 </div>
 
             </div>
-            <div>
-                <p><small><strong>Status: </strong> {props.status} </small></p>
-                <p><small><strong>Next Appointment: </strong> + {props.next_appointment} days ago<em>(Feb 19, 2021 / Friday)</em></small></p>
-                <p><small><strong>Last Appointment: </strong>  {props.prev_appointment} days ago <em>(Feb 12, 2021 / Thursday)</em></small></p>
-                <p></p>
+
+            <div className={classes.Client_Right_Section}>
+                <div className={classes.Client_status_div}>
+                    <p><small><strong>Status: </strong> {props.status} </small></p>
+                    <p><small><strong>Next Appointment: </strong> + {props.next_appointment} days ago<em>(Feb 19, 2021 / Friday)</em></small></p>
+                    <p><small><strong>Last Appointment: </strong>  {props.prev_appointment} days ago <em>(Feb 12, 2021 / Thursday)</em></small></p>
+                    <p></p>
+                </div>
+
+                <div className={classes.Client_Detail_Button_Div}>
+                    <button>Treatment Notes</button>
+                    <button>Payment Details</button>
+                </div>
+
             </div>
+
+
+
 
         </div>
     )
